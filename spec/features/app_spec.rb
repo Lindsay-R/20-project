089c5c01716249de82914f27e14be38fb68369e5
Lindsay-R <lrecknell@gmail.com>
2014-07-18 15:17:52 -0600

    made some tests pass

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -31,11 +31,17 @@ end #feature end
 feature "Basics: Userpage." do
   scenario "check userpage" do
     visit "/"
-    click_link "SignIn"
+    click_link "Join"
+    fill_in "Email", with: "lindsay@example.com"
+    fill_in "Username", with: "lindsay"
+    fill_in "Create Password", with: "123"
+    click_button "Join"
     fill_in "Email", with: "lindsay@example.com"
     fill_in "Password", with: "123"
     click_button "SignIn"
+    save_and_open_page
     expect(page).to have_link("Link")
+
     expect(page).to have_content "Lindsay"
     expect(page).to have_button("Search")
   end
